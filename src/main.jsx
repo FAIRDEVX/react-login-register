f075075510ce6eafafcda44a1e245044de191aba
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-08-22 18:15:13 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+import MyLogin from './components/MyLogin.jsx'
+import MyHome from './components/MyHome.jsx'
+import MyRegister from './components/MyRegister.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <MyLogin />
+  },
+  {
+    path: "/mylogin",
+    element: <MyLogin />
+  },
+  {
+    path: "/myregister",
+    element: <MyRegister />
+  },
+  {
+    path: "/myhome",
+    element: <MyHome />
   }
 ])
 
